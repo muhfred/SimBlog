2c67ddd9e3b2aeb01f1c4e66a2be235350cad0a1
muhfred <muhfred@outlook.com>
2016-11-09 09:25:45 +0200

    added JQuery and Bootstrap

--- BlogApp/Global.asax.cs
+++ BlogApp/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace BlogApp
@@ -12,7 +13,9 @@ namespace BlogApp
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
