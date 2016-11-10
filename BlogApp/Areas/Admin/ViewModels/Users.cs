462493b47e53a692ba5ddc0fca48c4bacd3e025e
muhfred <muhfred@outlook.com>
2016-11-10 23:48:00 +0200

    added transcation filter added user admin functionalities

--- BlogApp/Areas/Admin/ViewModels/Users.cs
+++ BlogApp/Areas/Admin/ViewModels/Users.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using BlogApp.Models;
@@ -9,5 +10,31 @@ namespace BlogApp.Areas.Admin.ViewModels
     public class UsersIndex
     {
         public IEnumerable<User> Users { get; set; }
+    }
+
+    public class UsersNew
+    {
+        [Required, MaxLength(128)]
+        public string Username { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required, MaxLength(256), DataType(DataType.EmailAddress)]
+        public string Email { get; set; }   
+
+    }
+
+    public class UserEdit
+    {
+        [Required, MaxLength(128)]
+        public string Username { get; set; }
+        [Required, MaxLength(256), DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+
+    public class UserResetPassword
+    {
+        public string Username { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
---
+++ BlogApp/Infrastructure/TransactionFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace BlogApp.Infrastructure
+{
+    public class TransactionFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Database.Session.BeginTransaction();
+        }
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if(filterContext.Exception == null)
+                Database.Session.Transaction.Commit();
+            else
+                Database.Session.Transaction.Rollback();
+        }
+
+        
+    }
+}
