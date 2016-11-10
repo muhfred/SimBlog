8101c8c0439fa771c517313a7c56b9296bc32ce5
muhfred <muhfred@outlook.com>
2016-11-10 06:41:05 +0200

    added and configured hibernate

--- BlogApp/Areas/Admin/Controllers/UsersController.cs
+++ BlogApp/Areas/Admin/Controllers/UsersController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BlogApp.Areas.Admin.ViewModels;
 using BlogApp.Infrastructure;
+using BlogApp.Models;
+using NHibernate.Linq;
 
 namespace BlogApp.Areas.Admin.Controllers
 {
@@ -14,7 +17,10 @@ namespace BlogApp.Areas.Admin.Controllers
         // GET: Admin/Users
         public ActionResult Index()
         {
-            return View();
+            return View(new UsersIndex()
+            {
+                Users = Database.Session.Query<User>().ToList()
+            });
         }
     }
 }
---
+++ BlogApp/Database.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+using BlogApp.Models;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Mapping.ByCode;
+
+namespace BlogApp
+{
+    public static class Database
+    {
+        private const string SessionKey = "BlogApp.Database.SessionKey";
+
+        private static ISessionFactory _sessionFactory;
+
+        public static ISession Session
+        {
+            get { return (ISession) HttpContext.Current.Items[SessionKey]; }
+        }
+
+        public static void Configure()
+        {
+            var config = new Configuration();
+
+            //configure our connection string
+            config.Configure();
+
+            //add our mappings
+            var mapper = new ModelMapper();
+            mapper.AddMapping<UserMap>();
+
+            config.AddMapping(mapper.CompileMappingForAllExplicitlyAddedEntities());
+
+            //create session factory
+            _sessionFactory = config.BuildSessionFactory();
+        }
+
+        public static void OpenSession()
+        {
+            HttpContext.Current.Items[SessionKey] = _sessionFactory.OpenSession();
+        }
+
+        public static void CloseSession()
+        {
+            var session = HttpContext.Current.Items[SessionKey] as ISession;
+            if (session != null)
+                session.Close();
+
+            HttpContext.Current.Items.Remove(SessionKey);
+            
+        }
+    }
+}
