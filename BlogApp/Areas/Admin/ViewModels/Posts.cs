3c1162bf7ec341d3856e70b68c48245cd4dd7c7f
muhfred <muhfred@outlook.com>
2016-11-13 10:38:40 +0200

    created new and edit actions of admin posts

--- BlogApp/Areas/Admin/ViewModels/Posts.cs
+++ BlogApp/Areas/Admin/ViewModels/Posts.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using BlogApp.Infrastructure;
 using BlogApp.Models;
 
@@ -8,5 +9,20 @@ namespace BlogApp.Areas.Admin.ViewModels
     public class PostsIndex
     {
            public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsForm
+    {
+        public bool IsNew { get; set; }
+        public int? PostId { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Slug { get; set; }
+
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
     }
 }
