14749c6b1092d4de11642242200bdb6760c2970d
muhfred <muhfred@outlook.com>
2016-11-15 09:18:28 +0200

    front-end posts controller

---
+++ BlogApp/ViewModels/Posts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BlogApp.Infrastructure;
+using BlogApp.Models;
+
+namespace BlogApp.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsShow
+    {
+        public Post Post { get; set; }
+    }
+
+    public class PostsTag
+    {
+        public Tag Tag { get; set; }
+        public PagedData<Post> Posts { get; set; }
+    }
+}
